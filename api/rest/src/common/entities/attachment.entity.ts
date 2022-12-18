467de23be7bf85fdfc8704dcc0247f84db81411c
jokercodes <chourasiavishal35@gmail.com>
2022-12-18 21:39:40 +0530

    entity change

--- api/rest/src/common/entities/attachment.entity.ts
+++ api/rest/src/common/entities/attachment.entity.ts
@@ -1,6 +1,6 @@
 import { CoreEntity } from 'src/common/entities/core.entity';
 
-export class Attachment extends CoreEntity {
+export class Attachment {
   thumbnail?: string;
   original?: string;
 }
